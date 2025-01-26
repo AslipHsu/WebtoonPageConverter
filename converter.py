62455690e756e322e743e9ba3b44291971a49b24
AslipHsu <bearlinm8866@gmail.com>
2025-01-27 04:03:48 +0800

    first commit

---
+++ converter.py
@@ -0,0 +1,39 @@
+
+import cv2
+needleImage="./pic/1.jpg"
+needleImage2="./pic/2.jpg"
+# https://m.ac.qq.com/comic/index/id/655106 (webtoon)
+# https://tw.manhuagui.com/comic/51466/ (book)
+
+# 800x2500
+# 938x1331
+nimg = cv2.imread(needleImage)
+
+# 縮小 50%
+scale_factor = 0.5
+original_height, original_width = nimg.shape[:2]
+print("n shape:",original_height,"x", original_width)
+new_width = int(original_width * scale_factor)
+new_height = int(original_height * scale_factor)
+new_size = (new_width, new_height)
+resized_image = cv2.resize(nimg, new_size, interpolation=cv2.INTER_AREA)#INTER_AREA插值
+resized_height, resized_width = resized_image.shape[:2]
+# 裁切
+crop_factor = int(0.5*resized_height)
+print("a:",crop_factor,"b:", len(resized_image[0]))
+crop_image_1=resized_image[0:crop_factor,:]
+crop_image_2=resized_image[crop_factor:,:]  
+
+# 合併00
+resized_image_2=cv2.hconcat([crop_image_1, crop_image_2])
+
+
+
+cv2.imshow("s2",resized_image_2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+# 1.合併
+# 2.切掉全黑or全白的row=>分出單格圖片
+# 3.單格合併成一張
