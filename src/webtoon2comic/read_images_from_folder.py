817c89076317afc4667b8797bdd0bcbe95c039e8
AslipHsu <bearlinm8866@gmail.com>
2025-01-31 10:04:56 +0800

    resolve conflicts

--- src/webtoon2comic/read_images_from_folder.py
+++ src/webtoon2comic/read_images_from_folder.py
@@ -3,11 +3,12 @@ from pathlib import Path
 from PIL import Image
 from PIL.Image import Image as ImageType
 
+import re
 
 def read_images_from_folder(input_folder: Path) -> list[ImageType]:
     """Read all image files from a folder."""
     images: list[ImageType] = []
-    for file_name in sorted(input_folder.iterdir()):
+    for file_name in sorted(input_folder.iterdir(), key=lambda x: int(re.search(r'\d+', x.name).group())):
         if file_name.suffix.lower() in {".png", ".jpg", ".jpeg"}:
             with Image.open(file_name) as image:
                 images.append(image.copy())
